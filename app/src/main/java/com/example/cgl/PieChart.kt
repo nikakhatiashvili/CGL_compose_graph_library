f45b570d787166ec0b8e5106c744cb2aa1603917
nika <khatiashvilinika44@gmail.com>
2024-02-02 21:59:26 +0400

    created screen

--- app/src/main/java/com/example/cgl/PieChart.kt
+++ app/src/main/java/com/example/cgl/PieChart.kt
@@ -2,15 +2,28 @@ package com.example.cgl
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.cgl.ui.theme.CGLTheme
 
 
 @Composable
-@Preview
 fun PieChartScreen() {
     Column(modifier = Modifier.fillMaxSize()) {
+        Text("asdasd")
+    }
+}
 
+fun pieChart(){
+
+}
+
+@Composable
+@Preview(showBackground = true)
+fun previewPieChartScreen(){
+    CGLTheme() {
+        PieChartScreen()
     }
 }
