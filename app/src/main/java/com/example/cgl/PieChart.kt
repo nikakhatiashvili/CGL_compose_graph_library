22c99571d48b282db6ae2cfe15079fd529fa54bb
nika <khatiashvilinika44@gmail.com>
2024-02-02 21:42:52 +0400

    Initial commit

---
+++ app/src/main/java/com/example/cgl/PieChart.kt
@@ -0,0 +1,16 @@
+package com.example.cgl
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+
+@Composable
+@Preview
+fun PieChartScreen() {
+    Column(modifier = Modifier.fillMaxSize()) {
+
+    }
+}
