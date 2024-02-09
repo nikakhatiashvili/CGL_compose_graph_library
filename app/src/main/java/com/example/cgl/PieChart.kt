2d4001f3c89f90110c8b0fae58409f47c441de83
nika <khatiashvilinika44@gmail.com>
2024-02-09 22:36:41 +0400

    added animation

--- app/src/main/java/com/example/cgl/PieChart.kt
+++ app/src/main/java/com/example/cgl/PieChart.kt
@@ -1,8 +1,9 @@
 package com.example.cgl
 
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.graphics.Canvas
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,6 +13,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
@@ -24,48 +27,27 @@ import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.graphics.drawscope.DrawScope
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.res.imageResource
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Density
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.graphics.drawable.DrawableCompat
 import com.example.cgl.model.PieChartSegment
 import com.example.cgl.ui.theme.*
 import kotlin.math.cos
 import kotlin.math.sin
 
 
+
 @Composable
 fun PieChartScreen() {
     Column(modifier = Modifier.fillMaxSize()) {
         Spacer(modifier = Modifier.height(200.dp))
-        val density = LocalDensity.current
-        val imageBitmap = vectorResourceToImageBitmap(R.drawable.transaction_svgrepo_com, density)
-        val shop = vectorResourceToImageBitmap(R.drawable.shopping_cart_02_svgrepo_com, density)
-        val bag = vectorResourceToImageBitmap(R.drawable.shopping_bag_svgrepo_com, density)
-        val fork = vectorResourceToImageBitmap(R.drawable.fork_and_knife_combination_svgrepo_com, density)
-        val edu = vectorResourceToImageBitmap(R.drawable.education_cap_svgrepo_com, density)
-        val bills = vectorResourceToImageBitmap(R.drawable.house_svgrepo_com, density)
 
-        val pieChartData3 = listOf(
-            PieChartSegment(color = red, percentage = 46f, imageRes = imageBitmap),
-            PieChartSegment(color = cyan, percentage = 16f,imageRes = shop),
-            PieChartSegment(color = lightGreen, percentage = 10f, imageRes = fork),
-//            PieChartSegment(color = yellow, percentage = 9f, imageRes = bag),
-//            PieChartSegment(color = darkPink, percentage = 8f, imageRes = edu),
-//            PieChartSegment(color = orange, percentage = 6f, imageRes = bills),
-//            PieChartSegment(color = gray, percentage = 2f),
-//            PieChartSegment(color = lightYellow, percentage = 1.5f),
-//            PieChartSegment(color = lightGray, percentage = 1f),
-        )
+
+        val pieChartData3 = getChartData()
         Box(modifier = Modifier.align(Alignment.CenterHorizontally)) {
             Column(
                 modifier = Modifier.align(Alignment.Center),
@@ -78,7 +60,8 @@ fun PieChartScreen() {
                 modifier = Modifier
                     .size(width = 300.dp, height = 270.dp),
                 pieChartData3,
-                30f
+                30f,
+                true
             )
         }
 
@@ -89,35 +72,63 @@ fun PieChartScreen() {
 fun PieChart(
     modifier: Modifier = Modifier,
     segments: List<PieChartSegment>,
-    thickness: Float
+    thickness: Float,
+    animateChart: Boolean = false
 ) {
+    val animatedSweepAngle = remember { Animatable(0f) }
+
+    if (animateChart) {
+        LaunchedEffect(key1 = "pie_chart_animation") {
+            animatedSweepAngle.snapTo(0f)
+            animatedSweepAngle.animateTo(
+                targetValue = 360f,
+                animationSpec = tween(durationMillis = 1000, delayMillis = 0)
+            )
+        }
+    }
 
     Box(modifier = modifier) {
-
         Canvas(modifier = modifier) {
-            val totalValue = segments.map { it.percentage }.sum()
-
             var startAngle = -90f
             val outerRadius = minOf(size.width, size.height) / 2f
             val innerRadius = outerRadius * (1 - thickness / 100)
+            val totalValue = segments.map { it.percentage }.sum()
 
-            segments.forEachIndexed { index, segment ->
-                val sweepAngle = if (totalValue > 0f) {
+            segments.forEach { segment ->
+                val segmentSweepAngle = if (totalValue > 0f) {
                     (segment.percentage / totalValue) * 360f
                 } else {
                     0f
                 }
 
-                drawPieChartSegment(
-                    startAngle = startAngle,
-                    sweepAngle = sweepAngle,
-                    innerRadius = innerRadius,
-                    outerRadius = outerRadius,
-                    segment = segment,
-                    imageBitmap = segment.imageRes
-                )
+                if (animateChart) {
+                    val currentSweepAngle = minOf(
+                        segmentSweepAngle,
+                        animatedSweepAngle.value - (startAngle + 90f)
+                    )
 
-                startAngle += sweepAngle
+                    if (animatedSweepAngle.value >= startAngle + 90f) {
+                        drawPieChartSegment(
+                            startAngle = startAngle,
+                            sweepAngle = currentSweepAngle,
+                            innerRadius = innerRadius,
+                            outerRadius = outerRadius,
+                            segment = segment,
+                            imageBitmap = segment.imageRes
+                        )
+                    }
+                } else {
+                    drawPieChartSegment(
+                        startAngle = startAngle,
+                        sweepAngle = segmentSweepAngle,
+                        innerRadius = innerRadius,
+                        outerRadius = outerRadius,
+                        segment = segment,
+                        imageBitmap = segment.imageRes
+                    )
+                }
+
+                startAngle += segmentSweepAngle
             }
         }
     }
@@ -183,21 +194,42 @@ fun DrawScope.drawPieChartSegment(
     val midAngle = startAngle + sweepAngle / 2
     val midAngleRadians = Math.toRadians(midAngle.toDouble())
 
-    // Calculating the position for the image
-    val iconRadius = (innerRadius + outerRadius) / 2 // Place the image in the middle of the thickness
+    val iconRadius = (innerRadius + outerRadius) / 2
     val iconX = (size.width / 2 + iconRadius * cos(midAngleRadians)).toFloat()
     val iconY = (size.height / 2 + iconRadius * sin(midAngleRadians)).toFloat()
 
-    // Draw the image at the center of the segment
     imageBitmap?.let {
-        val imageSize = Size(24.dp.toPx(), 24.dp.toPx()) // You can adjust the size as needed
+        val imageSize = Size(24.dp.toPx(), 24.dp.toPx())
         val imageTopLeft = Offset(iconX - imageSize.width / 2, iconY - imageSize.height / 2)
         drawImage(
             image = it,
             topLeft = imageTopLeft,
-            alpha = 1.0f // Use full opacity for icons by default
+            alpha = 1.0f
         )
     }
+}
+
+@Composable
+fun getChartData(): List<PieChartSegment> {
+    val density = LocalDensity.current
+    val imageBitmap = vectorResourceToImageBitmap(R.drawable.transaction_svgrepo_com, density)
+    val shop = vectorResourceToImageBitmap(R.drawable.shopping_cart_02_svgrepo_com, density)
+    val bag = vectorResourceToImageBitmap(R.drawable.shopping_bag_svgrepo_com, density)
+    val fork = vectorResourceToImageBitmap(R.drawable.fork_and_knife_combination_svgrepo_com, density)
+    val edu = vectorResourceToImageBitmap(R.drawable.education_cap_svgrepo_com, density)
+    val bills = vectorResourceToImageBitmap(R.drawable.house_svgrepo_com, density)
+    return         listOf(
+        PieChartSegment(color = Color.Black, percentage = 46f, imageRes = imageBitmap),
+        PieChartSegment(color = cyan, percentage = 16f,imageRes = shop),
+        PieChartSegment(color = lightGreen, percentage = 10f, imageRes = fork),
+        PieChartSegment(color = yellow, percentage = 9f, imageRes = bag),
+        PieChartSegment(color = darkPink, percentage = 8f, imageRes = edu),
+        PieChartSegment(color = orange, percentage = 6f, imageRes = bills),
+        PieChartSegment(color = gray, percentage = 2f),
+        PieChartSegment(color = lightYellow, percentage = 1.5f),
+        PieChartSegment(color = Color.Magenta, percentage = 1f),
+    )
+
 }
 
 @Composable
