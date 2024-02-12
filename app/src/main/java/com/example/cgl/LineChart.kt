50e18ed99d3f7d6aafdd66f59cc740e09756e2e2
nika <khatiashvilinika44@gmail.com>
2024-02-12 21:57:55 +0400

    added drawing line chart

--- app/src/main/java/com/example/cgl/LineChart.kt
+++ app/src/main/java/com/example/cgl/LineChart.kt
@@ -10,20 +10,13 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.cgl.ui.theme.CGLTheme
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.nativeCanvas
 import com.example.cgl.model.LineChartDataPoint
 import kotlin.math.roundToInt
@@ -36,9 +29,11 @@ fun LineChartScreen() {
         val dataPoints = listOf(
             LineChartDataPoint(time = 1, value = 50000f),
             LineChartDataPoint(time = 2, value = 48000f),
-            LineChartDataPoint(time = 3, value = 51000f),
-            LineChartDataPoint(time = 4, value = 53000f),
-            LineChartDataPoint(time = 5, value = 52000f)
+            LineChartDataPoint(time = 3, value = 18000f),
+            LineChartDataPoint(time = 4, value = 51000f),
+            LineChartDataPoint(time = 5, value = 53000f),
+            LineChartDataPoint(time = 6, value = 52000f),
+            LineChartDataPoint(time =8, value = 62000f),
         )
         Box(modifier = Modifier.align(Alignment.CenterHorizontally)) {
             LineChart(
@@ -66,49 +61,105 @@ fun LineChart(
 
     val labelValues = calculateLabelValues(yAxisMin, yAxisMax, 6)
 
-    Canvas(modifier = modifier) {
-        val width = size.width
-        val height = size.height
-        val stepX = width / (times.maxOrNull() ?: (1f - 1f))
+    Box(){
 
-        val labelXPosition = 10f
-        val axisLineXPosition = labelXPosition + 100f
-        labelValues.forEach { value ->
-            val yPosition = (1f - (value - yAxisMin) / (yAxisMax - yAxisMin)) * height
-            drawContext.canvas.nativeCanvas.drawText(
-                "$value",
-                labelXPosition,
-                yPosition,
-                android.graphics.Paint().apply {
-                    textSize = 30f
-                    color = android.graphics.Color.BLACK
-                }
-            )
-        }
+        Canvas(modifier = modifier) {
+            val width = size.width
+            val height = size.height
 
-        drawLine(
-            color = Color.Black,
-            start = Offset(x = axisLineXPosition, y = 0f),
-            end = Offset(x = axisLineXPosition, y = height),
-            strokeWidth = Stroke.DefaultMiter
-        )
+            val paint = android.graphics.Paint().apply {
+                textSize = 30f
+                color = android.graphics.Color.BLACK
+            }
+            val stepX = width / (times.maxOrNull() ?: (1f - 1f))
 
-        val points = dataPoints.map { dataPoint ->
-            val x = (dataPoint.time - 1) * stepX + axisLineXPosition + 10f
-            val y = (1f - (dataPoint.value - yAxisMin) / (yAxisMax - yAxisMin)) * height
-            Offset(x, y)
-        }
+            val labelXPosition = 10f
+            val axisLineXPosition = labelXPosition + 100f
 
-        for (i in 0 until points.size - 1) {
+            val topLabel = labelValues.first()
+            val topLabelYPosition = paint.textSize
+
+            val bottomLabel = labelValues.last()
+            val bottomLabelYPosition = height - paint.textSize
+
+            val stepYPosition = (bottomLabelYPosition - topLabelYPosition) / (labelValues.size - 1)
+
+            labelValues.reversed().forEachIndexed { index, value ->
+                val yPosition = topLabelYPosition + (stepYPosition * index) + 10f
+                drawContext.canvas.nativeCanvas.drawText(
+                    "$value",
+                    labelXPosition,
+                    yPosition,
+                    paint
+                )
+            }
+
             drawLine(
-                start = points[i],
-                end = points[i + 1],
-                color = Color.Blue,
+                color = Color.Black,
+                start = Offset(x = axisLineXPosition, y = 0f),
+                end = Offset(x = axisLineXPosition, y = height),
                 strokeWidth = Stroke.DefaultMiter
             )
+            val points = dataPoints.map { dataPoint ->
+                val x = (dataPoint.time - 1) * stepX + axisLineXPosition
+                val y = (1f - (dataPoint.value - yAxisMin) / (yAxisMax - yAxisMin)) * height
+                Offset(x, y)
+            }
+            for (i in 0 until points.size) {
+                drawCircle(color = Color.Blue, radius = 15f, center = points[i])
+            }
+            drawBaselineAndLabels(
+                drawScope = this,
+                paint = paint,
+                dataPoints = dataPoints,
+                stepX = stepX,
+                axisLineXPosition = axisLineXPosition,
+                bottomLabelYPosition = bottomLabelYPosition,
+                height = size.height
+            )
+
+            for (i in 0 until points.size - 1) {
+                drawLine(
+                    start = points[i],
+                    end = points[i + 1],
+                    color = Color.Blue,
+                    strokeWidth = Stroke.DefaultMiter
+                )
+            }
         }
     }
 }
+
+private fun drawBaselineAndLabels(
+    drawScope: DrawScope,
+    paint: android.graphics.Paint,
+    dataPoints: List<LineChartDataPoint>,
+    stepX: Float,
+    axisLineXPosition: Float,
+    bottomLabelYPosition: Float,
+    height: Float
+) {
+    val baselineYPosition = height - 50f
+
+    drawScope.drawLine(
+        color = Color.Gray,
+        start = Offset(x = axisLineXPosition, y = baselineYPosition),
+        end = Offset(x = axisLineXPosition + stepX * (dataPoints.size - 1), y = baselineYPosition),
+        strokeWidth = Stroke.DefaultMiter
+    )
+
+    dataPoints.forEachIndexed { index, dataPoint ->
+        val x = (dataPoint.time - 1) * stepX + axisLineXPosition
+        val y = baselineYPosition + paint.textSize + 10f
+        drawScope.drawContext.canvas.nativeCanvas.drawText(
+            "${dataPoint.time}",
+            x,
+            y,
+            paint
+        )
+    }
+}
+
 
 fun calculateLabelValues(min: Float, max: Float, numLabels: Int): List<Int> {
     val range = max - min
